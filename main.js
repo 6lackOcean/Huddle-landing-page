09cafd5bcd0fcb011e23cec9bd6e4c5013970c29
6lackOcean <122749331+6lackOcean@users.noreply.github.com>
2023-01-28 12:19:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const RegisterButton = document.querySelector(".content_button");
 const DisplayPopup = document.querySelector(".popup");
 
-RegisterButton.addEventListener('click', () =>{
-    DisplayPopup.classList.add("opened");
-    setTimeout(()=>{DisplayPopup.classList.remove("opened")},5000);
+RegisterButton.addEventListener("click", () => {
+
+    if (DisplayPopup.hasAttribute("data-visible")) {
+        RegisterButton.setAttribute("aria-expanded", true);
+    }
+    else {
+        DisplayPopup.toggleAttribute("data-visible");
+        RegisterButton.setAttribute("aria-expanded", true);
+        setTimeout(() => {
+            DisplayPopup.removeAttribute("data-visible");
+            RegisterButton.setAttribute("aria-expanded", false);
+        }, 5000);
+    }
+
 
 });
